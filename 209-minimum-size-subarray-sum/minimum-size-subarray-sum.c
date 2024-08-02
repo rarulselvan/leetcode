b0485fb704187bf8122a705dc98c3179aa5a9006
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-08-02 15:43:55 +0530

    Time: 25 ms (67.99%) | Memory: 13 MB (7.37%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.c
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.c
@@ -6,13 +6,11 @@ int minSubArrayLen(int s, int* nums, int n)
     int left = 0;
     int sum = 0;
     for (int i = 0; i < n; i++)
-     {
+    {
         sum += nums[i];
-        printf("sum=%d\n", sum);
-        while (sum >= s) 
+        while (sum >= s) //this while loop will execute(left pointer slides forward) until sum=s
         {
-            //if(sum==s)            
-                ans = (ans > ((i- left)+1)) ? ((i - left)+1):ans;
+            ans = (ans > ((i- left)+1)) ? ((i - left)+1):ans;
             sum = sum-nums[left]; // remove the left most 
             left++;//and slide the window forward
         }
