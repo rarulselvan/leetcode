16bd9b4053ad9649d685df10ef75ed70baa43194
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-08-03 20:07:59 +0530

    Time: 50 ms (39.08%), Space: 10.7 MB (10.36%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.c
@@ -0,0 +1,22 @@
+int max(int a, int b)
+{
+    return a>b? a:b;
+}
+bool canJump(int* nums, int numsSize)
+{
+    if(numsSize==1)
+        return true;
+    int mx = 0;
+    for (int i = 0; i < numsSize; ++i)
+    {
+        if (mx < i)
+        {
+            //max jump can't reach the ith position...hence drop further calculation and exit
+            return false;
+        }
+        mx = max(mx, i + nums[i]); //max position can be rreached from here...
+        //that is we are already reached index i from 0
+        // so i+ the current position capacity to jump...
+    }
+    return true;
+}
