7ce72446149e74cabaffd0b1b58e2f9990fc40e5
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-08-05 11:31:00 +0530

    Time: 6 ms (47.14%), Space: 11.6 MB (6.57%) - LeetHub

--- 0112-path-sum/0112-path-sum.c
+++ 0112-path-sum/0112-path-sum.c
@@ -8,27 +8,26 @@
  */
 bool hasPathSum(struct TreeNode* root, int targetSum) 
 {
-    // Preorder traversal
-    //subtract the visited node from the sum
-
-    // handle the base case for the recursion to terminate
+    //recursive program
+    //pre-order traversal
     if(root==NULL)
         return false;
-
-    int result=0;
-    
     int currentSum=targetSum-root->val;
-    if(currentSum ==0 && root->left==0 && root->right==0)
-        return true;
 
-    int ans=0;
+    if(currentSum ==0 && root->left==NULL && root->right==NULL)
+        return true;
+    
+    int result=0;
+
     if(root->left)
     {
-        ans=hasPathSum(root->left, currentSum);
-        if(ans==true)
-            return true;
+        result=hasPathSum(root->left, currentSum);
+        if(result==true)
+            return result;
     }
     if(root->right)
-        ans=ans || hasPathSum(root->right, currentSum);
-    return ans;   
+        result= result || hasPathSum(root->right, currentSum);
+
+    return result;
+
 }
