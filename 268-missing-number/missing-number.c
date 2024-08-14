ed2c2077d425117a0925c6ce343a516aa5f7b5af
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-08-14 12:47:56 +0530

    Time: 11 ms (87.24%) | Memory: 8.7 MB (54.77%) - LeetSync

--- 268-missing-number/missing-number.c
+++ 268-missing-number/missing-number.c
@@ -6,8 +6,13 @@ int missingNumber(int* nums, int numsSize)
     //get the array_sum
     //result=total-array_sum
 //Approach 2:
+printf("%d",numsSize);
     int res=numsSize;
     for(int i=0; i<numsSize; i++)
+    {
+        //xor of two element is zero. xor or three element is also zero
+        //so xor the res(n), 0-n-1, nums, result is the missing number
         res=res^ i^nums[i];
+    }
     return res;
 }
