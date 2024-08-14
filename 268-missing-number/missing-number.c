8beb09aebeb5172b15721cc8dcf9dc1525915833
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-08-14 12:41:47 +0530

    Time: 16 ms (38.18%) | Memory: 8.7 MB (54.77%) - LeetSync

---
+++ 268-missing-number/missing-number.c
@@ -0,0 +1,13 @@
+int missingNumber(int* nums, int numsSize)
+{
+//Approach 1:
+    //get the biggest number
+    //get the total n*(n+1)/2
+    //get the array_sum
+    //result=total-array_sum
+//Approach 2:
+    int res=numsSize;
+    for(int i=0; i<numsSize; i++)
+        res^= i^nums[i];
+    return res;
+}
