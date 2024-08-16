a5dac67aa697dc360545d062c84ed7549fc8eb13
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-08-16 19:12:55 +0530

    Time: 86 ms (54.75%) | Memory: 13.8 MB (52.89%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.c
@@ -0,0 +1,38 @@
+void swap(int *a,int *b)
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+void maxHeapify(int arr[], int i, int N)
+{
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    int largest = i;
+ 
+    if (l < N && arr[l] > arr[largest])
+        largest = l;
+    if (r < N && arr[r] > arr[largest])
+        largest = r;
+    
+    if (largest != i)
+    {
+        swap(&arr[i], &arr[largest]);
+        maxHeapify(arr, largest, N);
+    }
+}
+
+
+int findKthLargest(int* arr, int N, int k)
+{
+    for (int i = (N / 2) -1; i >= 0; --i)
+        maxHeapify(arr, i, N);
+    
+    for(int i=0; i<k-1; i++)
+    {
+        arr[0]=arr[N-1-i];//move(overwrite) the last element into arr[0]
+        maxHeapify(arr, 0, N-i);//we are loosing elements
+    }
+    return arr[0];
+}
