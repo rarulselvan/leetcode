f58a3c1ab2b77ab924d0f749e4e1eb042c988cad
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-08-24 13:15:55 +0530

    Time: 12 ms (39.67%) | Memory: 10.7 MB (5.48%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.c
+++ 42-trapping-rain-water/trapping-rain-water.c
@@ -15,17 +15,19 @@ int trap(int* arr, int n)
     leftSeen[0]=arr[0];
     for(int i=1; i<n; i++)
     {
+        //construct an array gives a value highest from the left
         leftSeen[i]=max(leftSeen[i-1], arr[i]);
     }
     rightSeen[n-1]=arr[n-1];
     for(int i=n-2; i>=0; i--)
     {
+        //construct an array gives a value highest from the right
         rightSeen[i]=max(rightSeen[i+1], arr[i]);
     }
 
     for(int i=0; i<n; i++)
     {
-        //printf("%d...%d\n",leftSeen[i], rightSeen[i]);
+        //get the minium of leftseen and rightseen ..from the subtract the particular position
         water+= min(leftSeen[i],rightSeen[i])-arr[i]   ;   
     }
     return water;
