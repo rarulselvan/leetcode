ced1d462fb4bd4e23c02dafb6f02be02491b95c3
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-09-19 16:53:24 +0530

    Time: 58 ms (77.21%) | Memory: 27.5 MB (74.77%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,11 +5,13 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         int n = nums.size();
-        for (int i = 0; i < n - 2 && nums[i] <= 0; ++i) 
+        
+        for (int i = 0; i < n - 2 && nums[i] <= 0; i++) 
         {
-            if (i && nums[i] == nums[i - 1])
+            if (i >0 )// skip the consecutive elements if they are same
             {
-                continue;
+                if(nums[i] == nums[i - 1])
+                    continue;
             }
             int j = i + 1, k = n - 1;
             while (j < k) 
