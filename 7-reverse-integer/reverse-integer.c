0c9f06018e879be675e8fa9693b7fd94327ad31c
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-10-05 09:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (7.44%) - LeetSync

--- 7-reverse-integer/reverse-integer.c
+++ 7-reverse-integer/reverse-integer.c
@@ -1,10 +1,13 @@
 int reverse(int x) {
     int ans = 0;
-    for (; x != 0; x /= 10) {
+    //for (; x != 0; x /= 10)
+    while(x!=0)
+    {
         if (ans > INT_MAX / 10 || ans < INT_MIN / 10) {
             return 0;
         }
         ans = ans * 10 + x % 10;
+        x=x/10;
     }
     return ans;
 }
