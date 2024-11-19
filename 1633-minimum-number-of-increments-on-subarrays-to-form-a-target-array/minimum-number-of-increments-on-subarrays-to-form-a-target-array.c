11f2efab56cde605d5cc569c086aad6790095360
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-11-19 12:27:27 +0530

    Time: 389 ms (25.00%) | Memory: 15.3 MB (92.31%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.c
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.c
@@ -2,10 +2,12 @@ int minNumberOperations(int* target, int targetSize)
 {
 
     int operations = target[0];
+    printf("/n %d", operations);
     for (int i = 1; i < targetSize; i++) 
     {
         if (target[i] > target[i - 1])
         {
+            printf("incement...%d",target[i] - target[i - 1]);
             operations += target[i] - target[i - 1];
         }
     }
