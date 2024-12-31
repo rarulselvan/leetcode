d75e81c9080f6e6235d0f9f9ce987f16f55ba60a
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2024-12-31 19:54:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (52.04%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        
+        set< pair<int,int> > s;
+        int x=0,y=0;
+        s.insert({x,y});
+        for(char c: path)
+        {
+            if(c=='N') y++;
+            if(c=='E') x++;
+            if(c=='S')  y--;
+            if(c=='W')  x--;
+            if(s.count({x,y}))
+            return true;
+            s.insert({x,y});
+        }
+        return false;
+        
+    }
+};
