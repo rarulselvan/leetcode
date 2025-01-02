e1606554dff3035c724543fda67f707e0bab3c82
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-02 20:57:18 +0530

    Time: 40 ms (38.30%) | Memory: 61.4 MB (56.38%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -3,10 +3,14 @@ class Solution
 public:
     int findKthLargest(vector<int>& nums, int k) 
     {
+        // 2nd arg vector<int> --- elements are stored in vector
+        //greater<int >- convert into min q..smallest element is always on top
         priority_queue<int, vector<int>, greater<int>> minQ;
-        for (int x : nums) {
+        for (int x : nums)
+        {
             minQ.push(x);
-            if (minQ.size() > k) {
+            if (minQ.size() > k)
+            {
                 minQ.pop();
             }
         }
