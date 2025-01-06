fe771272851f8931795f7256b656aa19873f16f2
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-06 18:16:40 +0530

    Time: 1 ms (26.83%) | Memory: 20.1 MB (7.23%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& arr)
+    {
+        int n = arr.size();
+        unordered_map<int, int> freq;
+        vector<int> res;
+
+        // find frequency of each number
+        for (int ele : arr)
+            freq[ele]++;
+
+        // Iterate over each key-value pair in the hash map
+        for (auto it : freq)
+        {
+            int ele = it.first;
+            int cnt = it.second;
+
+            // Add the element to the result, if its frequency
+            // if greater than floor(n/3)
+            if (cnt > n / 3)
+                res.push_back(ele);
+        }
+
+       // if (res.size() == 2 && res[0] > res[1])
+         //   swap(res[0], res[1]);
+        return res;
+    }
+};
