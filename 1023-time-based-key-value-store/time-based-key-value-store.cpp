2b27f7b4bbcbf5a524a8cdf7d440e9b12551643e
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-10 18:19:10 +0530

    Time: 53 ms (63.24%) | Memory: 136.6 MB (56.30%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -1,29 +1,43 @@
 
- class TimeMap {
+class TimeMap 
+{
+private:
+    unordered_map<string, vector<pair<int, string>>> m;
 public:
-    TimeMap() {
+    TimeMap()
+    {
         
     }
     
-    void set(string key, string value, int timestamp) {
+    void set(string key, string value, int timestamp) 
+    {
         m[key].push_back({timestamp, value});
     }
     
-    string get(string key, int timestamp) {
-        if (m.find(key) == m.end()) {
+    string get(string key, int timestamp)
+    {
+        if (m.find(key) == m.end()) 
+        {
+            //not found
             return "";
         }
         
         int low = 0;
         int high = m[key].size() - 1;
         
-        while (low <= high) {
+        while (low <= high)
+        {
             int mid = low + (high - low) / 2;
-            if (m[key][mid].first < timestamp) {
+            if (m[key][mid].first < timestamp) 
+            {
                 low = mid + 1;
-            } else if (m[key][mid].first > timestamp) {
+            } 
+            else if (m[key][mid].first > timestamp)
+            {
                 high = mid - 1;
-            } else {
+            } 
+            else
+            {
                 return m[key][mid].second;
             }
         }
@@ -33,6 +47,5 @@ public:
         }
         return "";
     }
-private:
-    unordered_map<string, vector<pair<int, string>>> m;
+
 };
