db6ab0e362ad9f0d79ae6781a0113b9fb76b9318
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-10 18:44:44 +0530

    Time: 64 ms (40.86%) | Memory: 136.5 MB (74.51%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.cpp
+++ 1023-time-based-key-value-store/time-based-key-value-store.cpp
@@ -2,48 +2,48 @@
 class TimeMap 
 {
 private:
-    unordered_map<string, vector<pair<int, string>>> m;
+    unordered_map<string, vector<pair<string, int>>> keyValStore;
 public:
     TimeMap()
     {
         
     }
-    
     void set(string key, string value, int timestamp) 
     {
-        m[key].push_back({timestamp, value});
+        keyValStore[key].push_back({value, timestamp});
     }
     
     string get(string key, int timestamp)
     {
-        if (m.find(key) == m.end()) 
+        if (keyValStore.find(key) == keyValStore.end()) 
         {
             //not found
             return "";
         }
         
         int low = 0;
-        int high = m[key].size() - 1;
+        int high = keyValStore[key].size() - 1;
         
         while (low <= high)
         {
             int mid = low + (high - low) / 2;
-            if (m[key][mid].first < timestamp) 
+            if (keyValStore[key][mid].second < timestamp) 
             {
                 low = mid + 1;
             } 
-            else if (m[key][mid].first > timestamp)
+            else if (keyValStore[key][mid].second > timestamp)
             {
                 high = mid - 1;
             } 
             else
             {
-                return m[key][mid].second;
+                return keyValStore[key][mid].first;
             }
         }
         
-        if (high >= 0) {
-            return m[key][high].second;
+        if (high >= 0)
+        {
+            return keyValStore[key][high].first;
         }
         return "";
     }
