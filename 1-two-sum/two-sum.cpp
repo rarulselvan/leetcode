7a9f9a7b14efb4adbb517079aa66eb949128633b
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-11 13:33:24 +0530

    Time: 139 ms (6.64%) | Memory: 14.1 MB (57.41%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> ans;
+       // vector<int> *ans = new vector<int >;
+       for(int i=0; i<nums.size(); i++)
+        {
+            for(int j=i+1; j<nums.size(); j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    cout<<"found it"<<endl;
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
+{
+    for (int i = 0; i < numsSize; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++) 
+        {
+            if (nums[j] == target- nums[i])
+            {
+                int *ans = (int *) malloc(sizeof(int)*2);
+                ans[0] = i;
+                ans[1] = j;
+                *returnSize=2;
+                return ans;
+            }
+        }
+    }
+    return NULL;
+}
