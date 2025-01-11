4733eb0108512cb41016c25e8456bfee9e8b457f
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-11 14:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.41%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -3,8 +3,11 @@ public:
     int prefixCount(vector<string>& words, string pref)
     {
         int ans = 0;
-        for (auto& w : words) 
-            ans += w.find(pref) == 0;
+        for (string w : words) 
+        {
+            if(w.find(pref) == 0)//found the substring at oth index
+                ans=ans+1;
+        }
         return ans;
     }
 };
