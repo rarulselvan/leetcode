29497f523d981fbd803aca09e21daa124b55b61f
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-17 16:11:23 +0530

    Time: 3 ms (83.56%) | Memory: 11.9 MB (52.40%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,13 +6,17 @@ public:
         unordered_map<char, int> myMap;
         int res = 0;
         int j = 0;
-
+        // This is a sliding window problem
+        // j is left most pointer
+        // i is right most pointer
         for (int i = 0; i < s.size(); i++)
         {
             if (myMap.count(s[i]) > 0)
             {
-               j = max(j, myMap[s[i]] + 1);
+                // the charactor is already there, hence increment J
+                j = max(j, myMap[s[i]] + 1);
             }
+            //
             res = max(res, i - j + 1);
             myMap[s[i]] = i;
         }
