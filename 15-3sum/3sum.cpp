acede4714cec7e564d5b9c42cfed2f2e1b1d1329
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-18 17:31:50 +0530

    Time: 39 ms (95.50%) | Memory: 29 MB (80.48%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,6 +6,7 @@ public:
         vector<vector<int>> ans;
         int n = nums.size();
         
+        
         for (int i = 0; i < n - 2 && nums[i] <= 0; i++) 
         {
             if (i >0 )// skip the consecutive elements if they are same
@@ -13,32 +14,35 @@ public:
                 if(nums[i] == nums[i - 1])
                     continue;
             }
-            int j = i + 1, k = n - 1;
-            while (j < k) 
+            int left = i + 1, right = n - 1;
+            while (left < right) 
             {
-                int x = nums[i] + nums[j] + nums[k];
+                int x = nums[i] + nums[left] + nums[right];
                 if (x < 0) 
                 {
-                    ++j;
+                    left++;
                 } 
                 else if (x > 0) 
                 {
-                    --k;
+                    right--;
                 } 
                 else
                 {
-                    ans.push_back({nums[i], nums[j++], nums[k--]});
-                    while (j < k && nums[j] == nums[j - 1]) 
-                    {
-                        ++j;
-                    }
-                    while (j < k && nums[k] == nums[k + 1]) 
-                    {
-                        --k;
-                    }
+                    //found the triplet...push into vector
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    //..left++..right -- to continue search the left over array elements
+                    left++;
+                    right --;
+
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    
+                    while (left < right && nums[right] == nums[right + 1]) 
+                        right--;
                 }
             }
         }
         return ans;
     }
 };
+
