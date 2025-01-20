2da6acf2b63ff0561b31d347301562007591c7d6
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-20 17:34:53 +0530

    Time: 3 ms (7.62%) | Memory: 9.3 MB (30.79%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    int uniquePathsDP(int m, int n, vector<vector<int>> &dp) 
+    {
+        if(m==0)
+            return 1;
+        if(n==0)
+            return 1;
+        if(dp[m][n] !=-1)
+            return dp[m][n];
+
+        dp[m][n]=uniquePathsDP(m-1, n, dp)+uniquePathsDP(m, n-1, dp);
+        return dp[m][n];
+    }
+    int uniquePaths(int m, int n) 
+    {
+        vector<int> row(n+1, -1);
+	    // Initializing the 2-D vector
+	    vector<vector<int>> dp(m+1, row);
+
+        return uniquePathsDP(m-1, n-1, dp);
+    }
+};
