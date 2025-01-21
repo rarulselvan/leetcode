ba30f8b2f151b8786007421efda3838a59688f47
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-21 17:04:22 +0530

    Time: 4 ms (47.62%) | Memory: 17.1 MB (83.71%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -3,23 +3,20 @@ class Solution
 public:
     vector<string> topKFrequent(vector<string>& words, int k)
     {
-        
         unordered_map<string, int> countMap;
-
         for(string word:words)
             countMap[word]++;
 
-        //  creates a vector named temp and initializes it with the 
+        //  creates a vector named wordFrePair and initializes it with the 
         //  elements of the Unordered Map Freq
-        vector <pair<string, int>> temp(countMap.begin(), countMap.end());
+        vector <pair<string, int>> wordFrePair(countMap.begin(), countMap.end());
 
         // sort the vector based on the custom comparator function 
-        sort (begin(temp), end(temp),myComparator);
-        
+       // sort (begin(wordFrePair), end(wordFrePair),myComparator);
+       sort(wordFrePair.begin(), wordFrePair.end(), myComparator);
         
         // declare resultant vector which contains expected output
         vector <string> result;
-        
         // declare a varible idx to access temp elements
         int idx = 0;
 
@@ -28,23 +25,21 @@ public:
             // it variable will have the string and freq 
             // it.first contains string
             // it.second contains freq of that sring
-            auto it = temp[idx];
-
+            auto it = wordFrePair[idx];
             // inserting our string into our resultant vector
             result.push_back(it.first);
-
             // increamenting the idx variable
             // so that we can access the next element
             idx++;
         }
         return result;
+        
     }
     static bool myComparator (pair <string, int> &a, pair <string, int> &b)
-{
-  if (a.second == b.second){
-      return a.first < b.first;
-    }
+    {
+        if (a.second == b.second)
+            return a.first < b.first;
 
-  return a.second > b.second;
-}
+        return a.second > b.second;
+    }
 };
