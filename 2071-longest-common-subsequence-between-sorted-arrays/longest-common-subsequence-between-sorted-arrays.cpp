1929192554e555c17a7068187f532370035ba67f
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-22 15:54:53 +0530

    Time: 4 ms (35.56%) | Memory: 21.1 MB (35.56%) - LeetSync

---
+++ 2071-longest-common-subsequence-between-sorted-arrays/longest-common-subsequence-between-sorted-arrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> longestCommonSubsequence(vector<vector<int>>& arrays)
+    {
+        unordered_map<int, int> counter;
+        vector<int> res;
+        int n = arrays.size();
+        for (auto array : arrays)
+        {
+            for (auto e : array) {
+                counter[e] += 1;
+                if (counter[e] == n) {
+                    res.push_back(e);
+                }
+            }
+        }
+        return res;
+    }
+};
