57e35933996173549380a60a700f2663caff4926
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-28 18:51:52 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (32.14%) - LeetSync

---
+++ 1618-delete-n-nodes-after-m-nodes-of-a-linked-list/delete-n-nodes-after-m-nodes-of-a-linked-list.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteNodes(ListNode* head, int m, int n)
+    {
+        if(head==NULL)
+            return head;
+ 
+        ListNode *oriHead=head;
+        while(head)
+        {
+            int mcount=m;
+            int ncount=n;
+            ListNode *tmphead=NULL;
+            while(mcount && head)
+            {
+                tmphead=head;
+                head=head->next;
+                mcount--;
+            }
+           // if(head)
+               // cout <<head->val;
+            if(head)
+            {
+                
+                while(ncount && head)
+                {
+                    head=head->next;
+                    ncount--;
+                }
+               
+                tmphead->next=head;
+                if(head)
+                cout <<tmphead->val<<" ----> "<<head->val<<endl;
+            }
+        }
+        return oriHead;
+        
+    }
+};
