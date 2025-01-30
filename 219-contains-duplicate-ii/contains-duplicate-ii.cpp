bade5452bc3b157b914fec6445a5e7e37a7dbcf8
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-01-30 17:46:01 +0530

    Time: 60 ms (36.32%) | Memory: 81.3 MB (26.76%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
    bool containsNearbyDuplicate(vector<int>& nums, int k)
     {
@@ -7,6 +7,23 @@ public:
         {
             if (d.count(nums[i]) && i - d[nums[i]] <= k)
             {
+                return true;
+            }
+            d[nums[i]] = i;
+        }
+        return false;
+    }
+};
+*/
+
+class Solution {
+public:
+   bool containsNearbyDuplicate(vector<int>& nums, int k)
+    {
+        unordered_map<int, int> d;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (d.count(nums[i]) && i - d[nums[i]] <= k) {
                 return true;
             }
             d[nums[i]] = i;
