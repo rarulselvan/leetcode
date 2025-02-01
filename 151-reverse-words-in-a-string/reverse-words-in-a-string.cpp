cade8524387d5a01d62377aee4f6d9a4a7ae2682
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-01 15:03:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (28.64%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,34 +1,34 @@
 //using namespace string;
-class Solution {
-public:
-string reverseWords(string str)
-    {
-        // Split the string into words
-        istringstream ss(str);
-        string word; 
-        vector<string> arr;
-        
-        while (ss >> word) 
-            arr.push_back(word);
-
-        // Reverse array of words  
-        reverse(arr.begin(), arr.end());  
-      
-    // Join the words in reverse order  
-    string Str;// = "";  
-    for (string &s : arr)
-    { 
-        if(Str.empty())
-            Str += (s + ""); 
-        else
+class Solution 
+{
+    public:
+        string reverseWords(string str)
         {
-            Str += " "; 
-            Str += (s); 
+            // Split the string into words
+            stringstream ss(str);
+            string word; 
+            vector<string> arr;
+          
+            //get all the strings in the sentence "str"
+            while (ss >> word) 
+                arr.push_back(word);
+
+            // Reverse array of words  
+            reverse(arr.begin(), arr.end());  
+        
+            // Join the words in reverse order  
+            string res;// = "";  
+            for (string s : arr)
+            { 
+                if(res.empty())
+                    res += (s + ""); 
+                else
+                {
+                    res += " "; 
+                    res += (s); 
+                }
+
+            }
+            return res;
         }
-
-    }
-   // trim_right(Str);
-
-    return Str;
-    }
 };
