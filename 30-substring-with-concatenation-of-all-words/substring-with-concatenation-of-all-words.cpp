190a7ee5526309aae2fd82275cdde66528dfdd3e
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-01 15:48:21 +0530

    Time: 25 ms (49.92%) | Memory: 23.7 MB (82.68%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -10,22 +10,27 @@ public:
                 ++wordCount[word];
             
 
-        int stringSize = s.size(), wordCountSize = words.size(), wordSize = words[0].size();
+        int stringSize = s.size();
+        int wordCountSize = words.size();
+        int wordSize = words[0].size();
         vector<int> substrIndices;
 
         // Iterate over the string 's'.
-        for (int i = 0; i < wordSize; ++i) {
+        for (int i = 0; i < wordSize; ++i) 
+        {
             unordered_map<string, int> windowCount;
             int left = i, right = i;
             int totalCount = 0;
 
             // Slide a window over the string 's'.
-            while (right + wordSize <= stringSize) {
+            while (right + wordSize <= stringSize) 
+            {
                 string currentWord = s.substr(right, wordSize);
                 right += wordSize;
 
                 // Skip the current segment if the word is not in 'words'.
-                if (!wordCount.count(currentWord)) {
+                if (!wordCount.count(currentWord)) 
+                {
                     windowCount.clear();
                     left = right;
                     totalCount = 0;
@@ -37,7 +42,8 @@ public:
                 ++totalCount;
 
                 // If there are more occurrences of 'currentWord' in the window than in 'words', remove from the left.
-                while (windowCount[currentWord] > wordCount[currentWord]) {
+                while (windowCount[currentWord] > wordCount[currentWord]) 
+                {
                     string wordToRemove = s.substr(left, wordSize);
                     left += wordSize;
                     --windowCount[wordToRemove];
@@ -45,12 +51,10 @@ public:
                 }
 
                 // If the total count of words match and all words frequencies are as expected, add to result.
-                if (totalCount == wordCountSize) {
+                if (totalCount == wordCountSize) 
                     substrIndices.push_back(left);
-                }
             }
         }
-
         return substrIndices;
     }
 };
