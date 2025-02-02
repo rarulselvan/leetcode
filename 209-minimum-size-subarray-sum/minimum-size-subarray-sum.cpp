a0c7007ba02beb663423ec5f96fdf3879381c45e
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-02 20:17:15 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (71.49%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -4,24 +4,24 @@ public:
     {
         int n = nums.size();              
         int sum = 0;                
-        int ans = n + 1;                  
+        int ans = n + 1;     
+        int left=0;
 
-        // Two pointers, 'i' is the end of the current subarray
-        // 'j' is the start of the current subarray
-        for (int i = 0, j = 0; i < n; ++i) 
+        // Two pointers, left and right
+        for (int right = 0; right < n; right++) 
         {
-            sum = sum + nums[i];               // Increase the sum by the current element
+            sum = sum + nums[right];               // Increase the sum by the current element
           
             // While sum is not smaller than the target and start pointer 'j' has not reached the end
-            while (j < n && sum >= target) 
+            while (right < n && sum >= target) 
             {
-                ans = min(ans, i - j + 1); // Update the answer with the new minimum length
-                sum =sum-nums[j];
-                j++;  // Subtract the first element of the subarray and move 'j' right
+                ans = min(ans, (right - left)); // Update the answer with the new minimum length
+                sum =sum-nums[left]; //remove the left pointer
+                left++;  // Subtract the first element of the subarray and move the left pointer
             }
         }
       
         // If 'ans' didn't change, no valid subarray was found, return 0 ...Otherwise, return the length of the shortest subarray
-        return ans == n + 1 ? 0 : ans;
+        return ans == n + 1 ? 0 : ans+1;
     }
 };
