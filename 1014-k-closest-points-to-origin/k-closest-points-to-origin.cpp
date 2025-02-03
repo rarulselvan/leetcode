a79e3f5625ab9f72fa580ad50c33995085c3dfdf
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-03 17:10:41 +0530

    Time: 34 ms (75.18%) | Memory: 61.7 MB (95.34%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -3,7 +3,8 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
     {
         //sort and lamda function
-        sort(points.begin(), points.end(), [](auto& a, auto& b)
+       // sort(points.begin(), points.end(), [](auto& a, auto& b)
+        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b)
                 {
                     return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]);
                 }
