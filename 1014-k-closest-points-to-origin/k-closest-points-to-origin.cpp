642b6a189a3f3ae8dd1800ff1c19831535802b22
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-03 17:29:47 +0530

    Time: 834 ms (5.03%) | Memory: 240 MB (5.00%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -6,11 +6,12 @@ public:
         //sort using lamda function
         sort(points.begin(), points.end(), [](const vector<int> a, const vector<int> b)
                                             {
-                                                int x = (a[0] * a[0] + a[1] * a[1]);
-                                                int y = (b[0] * b[0] + b[1] * b[1]);
-                                                return x < y;
+                                               // int x = (a[0] * a[0] + a[1] * a[1]);
+                                               // int y = (b[0] * b[0] + b[1] * b[1]);
+                                               // cout << x <<"  "<< y<<endl;
+                                               // return x < y;
 
-                                                //return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]);
+                                                return (a[0] * a[0] + a[1] * a[1]) < (b[0] * b[0] + b[1] * b[1]);
                                             }
         );
         return vector<vector<int>>(points.begin(), points.begin() + k);
