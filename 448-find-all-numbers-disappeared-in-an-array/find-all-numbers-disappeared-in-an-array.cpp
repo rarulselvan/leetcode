c319320112e922a2a76ff045d1506060765f5687
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-05 10:01:35 +0530

    Time: 675 ms (5.01%) | Memory: 53 MB (73.54%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -6,25 +6,28 @@ public:
 
 //        sort(nums.begin(), nums.end());
         
-        while (i < nums.size()) {
+        //cyclic sort the array
+        while (i < nums.size()) 
+        {
             int j = nums[i] - 1;
 
-            if (nums[i] != nums[j]) {
-                // swap
-                std::swap(nums[i], nums[j]);
-            } else {
+            if (nums[i] != nums[j])
+                swap(nums[i], nums[j]);
+            else 
                 i++;
-            }
         }
+        for(auto x :nums)
+            cout <<x<< " ";
         
-        std::vector<int> missingNumbers;
+        vector<int> missingNumbers;
 
-        for (i = 0; i < nums.size(); i++) {
-            if (nums[i] != i + 1) {
+        for (i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != i + 1)
+            {
                 missingNumbers.push_back(i + 1);
             }
         }
-
         return missingNumbers;
     }
 };
