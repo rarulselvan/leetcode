3f972e4442a5af3c0f52713b57e5945eaba72f67
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-05 22:02:08 +0530

    Time: 124 ms (9.67%) | Memory: 11.9 MB (58.26%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,7 +1,7 @@
 class Solution 
 {
 public:
-    int lengthOfLongestSubstring(string s)
+/*    int lengthOfLongestSubstring(string s)
     {
         unordered_map<char, int> myMap;
         int res = 0;
@@ -11,14 +11,40 @@ public:
         // i is right most pointer
         for (int i = 0; i < s.size(); i++)
         {
+            // if the charactor is available in the map ....
             if (myMap.count(s[i]) > 0)
             {
-                // the charactor is already there, hence increment J
+                //move the j to the available charactor position...
+                cout << myMap[s[i]] << "  ";
                 j = max(j, myMap[s[i]] + 1);
             }
             //
             res = max(res, i - j + 1);
-            myMap[s[i]] = i;
+            myMap[s[i]] = i;// store the locations
+        }
+        return res;
+    }
+    */
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<int, int> Map;
+        int i=0;
+        int j=0;
+        int res=0;
+        
+        // Sliding window ... j...is right's position.... i is in the left's position
+        for(j=0; j<s.size(); j++)
+        {
+            // check the charactor is present in the map...
+            if(Map.count(s[j])>0)
+            {
+                //now move the i forward till that charactor position ... slide the window
+                i=max(i, Map[s[j]]+1); // max is required to handle "abba" case...
+                cout <<i<<" ";
+            }
+            res= max(res,j-i+1);//Nunmber of unique charactors ...
+
+            Map[s[j]]=j; //store the charactor and it's position in map
         }
         return res;
     }
