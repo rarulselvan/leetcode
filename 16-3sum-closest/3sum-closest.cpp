de86d8cda94c6af9480a7bca8e97220fc9ff0877
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-08 20:34:10 +0530

    Time: 15 ms (34.90%) | Memory: 14 MB (21.33%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target)
+    {
+        // three number can be anywhere in the array... need notbe consecutive .hence sort it
+        sort(nums.begin(),nums.end());
+        
+        int res = 0xFFFF;//1 << 30;
+
+        for(int i=0; i<nums.size()-2; i++)
+        {
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k)
+            {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum==target)
+                    return sum;
+                if (abs(target-sum) < abs(target-res))  //get the abs of sum and ans(previous sum) from target ... and get the minimim value
+                    res = sum;
+                if (sum < target)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        return res;
+    }
+};
