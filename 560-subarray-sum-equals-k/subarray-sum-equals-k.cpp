097f71210d1aef6250d742dd3a76907a2accceec
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-08 13:52:19 +0530

    Time: 41 ms (72.90%) | Memory: 51.1 MB (14.57%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,72 +1,26 @@
-/*
-class Solution {
+class Solution 
+{
 public:
-    int subarraySum(vector<int>& arr, int sum)
-    {
-        // unordered_set to store prefix sums.
-        unordered_set<int> prefixSums;
-        int res = 0;
-        int currSum = 0;
-
-        for (int i = 0; i < arr.size(); i++) 
+    int subarraySum(vector<int>& nums, int k)
+     {
+        unordered_map<int, int> prefixSumFrequency;
+        prefixSumFrequency[0] = 1; // Base case: there's one way to have a sum of 0 (no elements).
+      
+        int answer = 0;  
+        int cumulativeSum = 0; 
+      
+        for (int num : nums) 
         {
-            // Add current element to sum so far.
-            currSum += arr[i];
-
-            // If currSum is equal to desired sum, then a new
-            // subarray is found. So increase count of subarrays.
-            if (currSum == sum)
-                res++;
-
-            // If currSum exceeds the desired sum, check if 
-            // the difference  exists in the prefixSums set.
-           // if(currSum>sum)
-            {
-                if (prefixSums.find(currSum - sum) != prefixSums.end())
-                    res++;
-            }
-
-            // Add currSum to the set of prefix sums.
-            prefixSums.insert(currSum);
+            cumulativeSum += num; // Update the cumulative sum.
+            // If cumulativeSum - k exists in prefixSumFrequency, then a subarray ending at current
+            // index has a sum of k. We add the count of those occurrences to answer.
+            answer += prefixSumFrequency[cumulativeSum - k];
+            // We then increment the count of cumulativeSum in our frequency map.
+            prefixSumFrequency[cumulativeSum]++;
         }
-        return res;
+      
+        // Return the total count of subarrays that sum up to k.
+        return answer;
     }
 };
-
-*/
-
-class Solution {
-public:
-    int subarraySum(vector<int>& arr, int k)
-     {
-        int n = arr.size();
-        int prefix[n]; // make a prefix array to store prefix sum
-        
-        // create the prefix array
-        prefix[0] = arr[0];
-        for(int i = 1; i < n; i++)
-            prefix[i] = prefix[i - 1] + arr[i];
-        
-
-        map<int,int> mp; // declare an unordered map    
-        int ans = 0; // to store the number of our subarrays having sum as 'k'
-    
-        for(int i = 0; i < n; i++) // traverse from the prefix array
-        {
-            if(prefix[i] == k) // if it already becomes equal to k, then increment ans
-                ans++;
-            
-            // now, as we discussed find whether (prefix[i] - k) present in map or not
-            if(mp.find(prefix[i] - k) != mp.end())
-            {
-                cout << mp[prefix[i] - k] <<"...";
-                ans += mp[prefix[i] - k]; // if yes, then add it our answer
-               // ans=ans+1;
-            }
-            
-            mp[prefix[i]]++; // put prefix sum into our map
-        }
-        return ans; // and at last, return our answer
-    }
-}; 
 
