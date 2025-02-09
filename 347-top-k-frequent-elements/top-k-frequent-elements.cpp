0414eee61c0c40b66a86db1961900b6ff7fdbf2a
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-09 11:51:30 +0530

    Time: 4 ms (37.64%) | Memory: 17.9 MB (72.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) 
     {
@@ -6,8 +6,8 @@ public:
         for (int num : nums)
             Map[num]++;
         vector<pair<int, int>> arr;
-        for(auto x: Map)
-            arr.push_back({x.second, x.first});
+        for(pair<int, int> x: Map)
+            arr.push_back(x);//{x.second, x.first});
         
         sort(arr.rbegin(), arr.rend());
 
@@ -17,6 +17,44 @@ public:
         
         for(auto x : res)
             cout <<x <<" "; 
+        return res;
+     
+    }
+};*/
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+        unordered_map<int, int> Map;
+        for (int num : nums)
+            Map[num]++;
+
+        //priority_queue<T, Container, Comparator> is the general syntax.
+        //pair<int, int> is used as T.
+        //vector<pair<int, int>> is the container.
+        //greater<pair<int, int>> ensures a min-heap, sorting pairs by the first element (default behavior for pair).
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        //priority_queue<pair<int, int>> minHeap;
+        // Iterate over the frequency map.
+        for (const auto& element : Map) 
+        {
+            int number = element.first;
+            int frequency = element.second;
+            minHeap.push({frequency, number});// Push the frequency first and number second... for the heap comaprision
+
+            // If the heap size exceeds k, remove the least frequent element.This ensures that the heap always contains the top k frequent elements.
+            if (minHeap.size() > k)
+                minHeap.pop();
+        }
+        vector<int> res;
+        for(int i=0; i<k;i++)
+        {
+            auto x=minHeap.top();
+            res.push_back(x.second);
+            minHeap.pop();
+        }
         return res;
     }
 };
