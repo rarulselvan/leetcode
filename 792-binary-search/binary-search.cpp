2016b2d54c12ccef3e6b48ea7e30be20b0a2cc4c
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-10 11:54:55 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.08%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target)
-    {
-        int len=nums.size()-1;
+    int search(vector<int>& nums, int target) {
+        int len = nums.size() - 1;
         return binarysearch(nums, 0, len, target);   
     }
-    int binarysearch(vector<int>& nums,int low, int high, int target)
-    {
-        if(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(target==nums[mid])
-                return  mid;
-            else if(target>nums[mid])
-            {
-                 cout <<low<<"  "<<high<<endl;
-                return binarysearch(nums, mid+1,high, target);
-            }
-            else
-            {
-                cout <<low<<"  "<<high<<endl;
-                return binarysearch(nums, low, mid-1, target);
+
+    int binarysearch(vector<int>& nums, int low, int high, int target) {
+        if (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (target == nums[mid])
+                return mid;  // Return index
+
+            if (nums[low] <= nums[mid]) {  // Left part sorted
+                if (nums[low] <= target && target <= nums[mid])
+                    return binarysearch(nums, low, mid - 1, target);
+                else
+                    return binarysearch(nums, mid + 1, high, target);
+            }    
+            else {  // Right part sorted
+                if (nums[mid] <= target && target <= nums[high])
+                    return binarysearch(nums, mid + 1, high, target);
+                else
+                    return binarysearch(nums, low, mid - 1, target);
             }
         }
-        return -1;
+        return -1;  // Not found
     }
 };
