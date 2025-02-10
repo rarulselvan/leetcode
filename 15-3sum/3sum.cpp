4dfc98548bb5c87dce8a0c928ece4ea5b61e09ae
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-10 10:22:58 +0530

    Time: 49 ms (61.24%) | Memory: 31.9 MB (26.24%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,14 +5,14 @@ public:
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         int n = nums.size();
+
         
-        
-        for (int i = 0; i < n - 2 && nums[i] <= 0; i++) 
+        for (int i = 0; i < n - 2 /*&& nums[i] <= 0*/; i++) 
         {
             if (i >0 )// skip the consecutive elements if they are same
             {
                 if(nums[i] == nums[i - 1])
-                    continue;
+                   continue;
             }
             int left = i + 1, right = n - 1;
             while (left < right) 
@@ -29,21 +29,14 @@ public:
                     //..left++..right -- to continue search the left over array elements
                     left++;
                     right --;
-
-                   /* while (left < right && nums[left] == nums[left - 1])
-                        left++;
-                    
-                    while (left < right && nums[right] == nums[right + 1]) 
-                        right--;
-                        */
                 }
             }
         }
         sort(ans.begin(), ans.end());
         // Remove consecutive duplicate elements
-        auto it = unique(ans.begin(), ans.end());
-        // Erase the extra elements at the end
-        ans.erase(it, ans.end());
+       auto it = unique(ans.begin(), ans.end());
+        // Erase the duplicate vectors
+      ans.erase(it, ans.end());
         return ans;
     }
 };
