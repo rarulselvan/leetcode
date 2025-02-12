ef2048df1ba65831116a8adcaae031874f4e81d3
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-12 14:05:08 +0530

    Time: 21 ms (31.03%) | Memory: 19.7 MB (22.20%) - LeetSync

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    int minSteps(string s, string t)
+    {
+        unordered_map<char, int> Map;
+
+        for (auto i : s) 
+            Map[i]++;// Increasing the count if the no. is present
+        
+        int count=0;
+        for (auto x : t)
+        {
+            // Checking the charactors are present or not 
+            if (Map[x] > 0) 
+                Map[x]--;  // If present then decrease the no. in map by 1
+            
+            else
+                count++;    // If not present increase count by 1
+        }
+        cout << count;
+        return count;
+    }
+};
