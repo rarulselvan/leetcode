72b9852c0928eb38eef5d0cc62b7b6f4b8a317d5
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-17 13:16:14 +0530

    Time: 4 ms (13.44%) | Memory: 22.7 MB (79.56%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -8,37 +8,49 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution 
+{
 public:
     void reorderList(ListNode* head)
-    {
+     {
+        if (!head || !(head->next) )
+            return;// If the list has 0, 1, or 2 nodes, no reordering is needed.
+
+        // Use the fast and slow pointer technique to find the middle of the list.
         ListNode* fast = head;
         ListNode* slow = head;
         while (fast && fast->next)// && fast->next->next)
         {
-            slow = slow->next;
-            fast = fast->next->next;
+            slow = slow->next; // Move one step.
+            fast = fast->next->next; // Move two steps.
         }
 
-        ListNode* cur = slow->next;
-        slow->next = nullptr;
+        // Split the list into two halves.
+        ListNode* secondHalf = slow->next;
+        slow->next = nullptr; // Terminate first half.
 
-        ListNode* pre = nullptr;
-        while (cur)
-         {
-            ListNode* t = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = t;
+        // Reverse the second half of the list.
+        ListNode* prev = NULL;
+        while (secondHalf) 
+        {
+            ListNode* temp = secondHalf->next;
+            secondHalf->next = prev;
+            prev = secondHalf;
+            secondHalf = temp;
         }
-        cur = head;
 
-        while (pre) {
-            ListNode* t = pre->next;
-            pre->next = cur->next;
-            cur->next = pre;
-            cur = pre->next;
-            pre = t;
+        // Start merging the first and second halves one node at a time.
+        ListNode* firstHalf = head;
+        ListNode* secondHalfHead = prev; // Points to the head of the reversed second half.
+        while (secondHalfHead) 
+        {
+            ListNode* temp = secondHalfHead->next;
+            secondHalfHead->next = firstHalf->next;
+            firstHalf->next = secondHalfHead;
+
+            // Move pointers ahead.
+            firstHalf = secondHalfHead->next; // Moved to the next of the newly added node.
+            secondHalfHead = temp; // Moving to the next node in the reversed half.
         }
     }
 };
