bb2c7957140de9d13ea5fcf3073d4e3ec5a06723
rarulselvan <31684651+rarulselvan@users.noreply.github.com>
2025-02-19 09:08:39 +0530

    Time: 184 ms (10.50%) | Memory: 22.7 MB (6.55%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,35 @@
+class Solution 
+{
+private:
+    vector<int>dp;
+public:
+    int jump(vector<int>& nums)
+    {
+        dp.assign(nums.size() + 1, -1);
+        return  jumpDP(nums, 0, nums.size()-1);
+    }
+    int jumpDP(vector<int>& nums, int start, int end)
+    {
+        // Base case.. wec ould able to reach the end of array .. hence this is a valid path
+        if (start >= end)
+            return 0;
+
+        if (dp[start] != -1)
+            return dp[start];
+
+        int result = INT_MAX;
+
+        for (int i = 1; i <= nums[start]; i++)  // Fix: Correct loop condition
+        {
+            if(start + i > end)
+                break; // array boundary check
+
+            int minJumps = jumpDP(nums, start + i, end);
+            if (minJumps != INT_MAX) // Fix: Prevent overflow when adding 1
+                result = min(result, 1 + minJumps);
+        }
+        // at times nums[start] is zero INT_MAX will be returned
+        dp[start] = result;
+        return result;
+    }
+};
