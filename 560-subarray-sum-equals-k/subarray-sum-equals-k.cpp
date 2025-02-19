40e23bf9337948118261b2faa5cb9c3422b95489
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-02-19 10:49:19 +0530

    Time: 45 ms (63.95%) | Memory: 51 MB (24.71%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,26 +1,15 @@
 class Solution 
 {
 public:
-    int subarraySum(vector<int>& nums, int k)
-     {
-        unordered_map<int, int> prefixSumFrequency;
-        prefixSumFrequency[0] = 1; // Base case: there's one way to have a sum of 0 (no elements).
-      
-        int answer = 0;  
-        int cumulativeSum = 0; 
-      
-        for (int num : nums) 
-        {
-            cumulativeSum += num; // Update the cumulative sum.
-            // If cumulativeSum - k exists in prefixSumFrequency, then a subarray ending at current
-            // index has a sum of k. We add the count of those occurrences to answer.
-            answer += prefixSumFrequency[cumulativeSum - k];
-            // We then increment the count of cumulativeSum in our frequency map.
-            prefixSumFrequency[cumulativeSum]++;
-        }
-      
-        // Return the total count of subarrays that sum up to k.
-        return answer;
-    }
-};
+ int subarraySum(vector<int>& nums, int k) {
+    unordered_map<int, int> sumCount = {{0, 1}}; // Initialize with sum 0 having one occurrence
+    int cumulativeSum = 0, count = 0;
 
+    for (int num : nums) {
+        cumulativeSum += num;
+        count += sumCount[cumulativeSum - k]; // Add count of subarrays that sum to k
+        sumCount[cumulativeSum]++; // Update frequency of cumulativeSum
+    }
+    return count;
+}
+};
