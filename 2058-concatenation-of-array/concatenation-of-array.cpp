a58c90140c22b914b3d7a854bbc1bbd6ccd01827
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-02-19 13:33:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (18.02%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -7,14 +7,12 @@ public:
         vector<int>ans;
         for(auto x: nums)
             ans.push_back(x);
-            for(auto x: nums)
+        for(auto x: nums)
             ans.push_back(x);
         //for (auto x : reverse(nums))         
         //ans.push_back(x);
         //for (auto it = nums.rbegin(); it != nums.rend(); ++it)
           //  ans.push_back(*it);
-
-        
         return ans;
     }
 };
