0e70971e1a6245bc03def998e791a980adacb008
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-02-22 20:49:17 +0530

    Time: 127 ms (9.67%) | Memory: 11.9 MB (58.19%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,30 +1,6 @@
 class Solution 
 {
 public:
-/*    int lengthOfLongestSubstring(string s)
-    {
-        unordered_map<char, int> myMap;
-        int res = 0;
-        int j = 0;
-        // This is a sliding window problem
-        // j is left most pointer
-        // i is right most pointer
-        for (int i = 0; i < s.size(); i++)
-        {
-            // if the charactor is available in the map ....
-            if (myMap.count(s[i]) > 0)
-            {
-                //move the j to the available charactor position...
-                cout << myMap[s[i]] << "  ";
-                j = max(j, myMap[s[i]] + 1);
-            }
-            //
-            res = max(res, i - j + 1);
-            myMap[s[i]] = i;// store the locations
-        }
-        return res;
-    }
-    */
     int lengthOfLongestSubstring(string s)
     {
         unordered_map<char, int> Map;
@@ -39,7 +15,8 @@ public:
             if(Map.count(s[j])>0)
             {
                 //now move the i forward till that charactor position ... slide the window
-                i=max(i, Map[s[j]]+1); // max is required to handle "abba" case...
+                i=max(i, Map[s[j]]+1); // max is required to handle "abba" case... if we do not do "max" i  will move backward
+                //i=Map[s[j]]+1;
                 cout <<i<<" ";
             }
             res= max(res,j-i+1);//Nunmber of unique charactors ...
