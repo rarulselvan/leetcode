98585eed4462eef107dc413ef707df8e01757c8b
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-02-27 21:01:48 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (37.91%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,4 +1,4 @@
-class Solution
+/*class Solution
 {
 public:
     void findSubsets(vector<int>& v, int idx,
@@ -39,5 +39,35 @@ public:
             res.push_back(x);
 
         return res;
+    }
+}; */
+class Solution 
+{
+public:
+    vector<vector<int>> output;
+    int n;
+    
+    void backtrack(int first, vector<int> curr, vector<int>& nums) 
+    {
+        // Add the current combination to the output
+        output.push_back(curr);
+        
+        // Explore further combinations
+        for (int i = first; i < n; ++i) 
+        {
+            // Add the next element to the current combination
+            curr.push_back(nums[i]);
+            // Recurse to form the next combination
+            backtrack(i + 1, curr, nums);
+            // Backtrack and remove the last element
+            curr.pop_back();
+        }
+    }  
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        n = nums.size();
+        vector<int> curr;
+        backtrack(0, curr, nums); // Call once to generate all subsets
+        return output;
     }
 };
