4b369e7f5701992496da81f2f90d2ecdfde73839
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-03-02 20:10:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (98.12%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    // Function to add the digits of a number until the result is a single digit.
+    int addDigits(int num)
+     {
+    int sum = 0;
+
+    // Repetitively calculate sum until
+    // it becomes single digit
+    while (num > 0 || sum > 9) {
+
+        // If n becomes 0, reset it to sum 
+        // and start a new iteration.
+        if (num == 0) {
+            num = sum;
+            sum = 0;
+        }
+
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+    }
+};
