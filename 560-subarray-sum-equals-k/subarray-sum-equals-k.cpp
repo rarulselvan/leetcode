c2fa1da5c7339d867ade896a5b81b81cbfb16e0f
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-03-06 12:52:43 +0530

    Time: 502 ms (25.94%) | Memory: 51.1 MB (13.54%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,15 +1,48 @@
 class Solution 
 {
 public:
- int subarraySum(vector<int>& nums, int k) {
-    unordered_map<int, int> sumCount = {{0, 1}}; // Initialize with sum 0 having one occurrence
-    int cumulativeSum = 0, count = 0;
+     int subarraySum(vector<int>& nums, int k) 
+     {
+        unordered_map<int, int> map;  // HashMap to store cumulative sum frequencies
+        map[0] = 1;  // The first number may be equal to k ...hence this is required
+        
+        int count = 0, sum = 0;
 
-    for (int num : nums) {
-        cumulativeSum += num;
-        count += sumCount[cumulativeSum - k]; // Add count of subarrays that sum to k
-        sumCount[cumulativeSum]++; // Update frequency of cumulativeSum
+        for (int num : nums) 
+        {
+            sum += num;
+            //count += map[sum - k];  // Add count of subarrays that sum to k
+            cout <<sum<<" ";
+            if(map[sum-k])
+               count += map[sum - k];
+            map[sum]++;  // Update frequency of cumulative sum
+        }
+    
+        return count;
     }
-    return count;
-}
 };
+/*
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int target) 
+    {
+        int left = 0, sum = 0, count = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];  // Expand the window
+                        // If the sum matches the target, increase the count
+           
+            // Shrink the window from the left if sum exceeds target
+            while (left < right && sum > target) {
+                sum -= nums[left];
+                left++;
+            }
+
+            // If the sum matches the target, increase the count
+            if (sum == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+}; */
