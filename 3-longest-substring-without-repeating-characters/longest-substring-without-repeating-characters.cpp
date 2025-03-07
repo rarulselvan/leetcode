703aca389651c298b983568abb6a304f350e0344
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-03-07 11:43:50 +0530

    Time: 12 ms (46.13%) | Memory: 14.3 MB (30.55%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution 
 {
 public:
@@ -24,5 +25,41 @@ public:
             Map[s[j]]=j; //store the charactor and it's position in map...this will be overwritten
         }
         return res;
+    }
+};
+*/
+class Solution 
+{
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        // This unordered set is used to store the characters that are currently in the
+        // longest substring without repeating characters.
+        unordered_set<char> slidingSet;
+
+        // The starting index of the substring.
+        int start = 0;
+        int maxLength = 0;
+
+        for (int end = 0; end < s.size(); ++end) 
+        {
+            // If the character at the current ending index of the substring already exists
+            // in the character set, continue to remove characters from the start of the
+            // substring until the character is no longer in the set.
+            while (slidingSet.count(s[end])) 
+            {
+                slidingSet.erase(s[start]);
+                start += 1;
+            }
+
+            // Insert the current character into the set.
+            slidingSet.insert(s[end]);
+
+            // Calculate the length of the current substring and update maxLength
+            // if this length is the largest we've found so far.
+            maxLength = max(maxLength, end - start + 1);
+        }
+        // Return the length of the longest substring found.
+        return maxLength;
     }
 };
