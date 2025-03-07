54eb05b3aa20ff7e9a9edba79dec132ce986bc88
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-03-07 15:49:53 +0530

    Time: 59 ms (16.45%) | Memory: 78.5 MB (7.99%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) 
     {
@@ -11,6 +11,24 @@ public:
         for( i=1; i<= (nums.size()); i++)
         {
             if(Map[i]==false) return i;
+        }
+        return i;
+    }
+};
+*/
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) 
+    {
+        unordered_map<int, int> Map;
+        // Add all elements into hash map
+        for(int x: nums)
+            Map[x]=1;
+        
+        int i=1;
+        for( i=1; i<= (nums.size()); i++)
+        {
+            if(Map[i]==0) return i;
         }
         return i;
     }
