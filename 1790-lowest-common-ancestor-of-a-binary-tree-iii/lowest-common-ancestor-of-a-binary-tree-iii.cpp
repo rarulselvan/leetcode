69915ccb1f6ce56058c9ba9a9e0470ab95bc6ea1
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-03-22 20:49:04 +0530

    Time: 8 ms (79.03%) | Memory: 14.3 MB (54.75%) - LeetSync

---
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -0,0 +1,27 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution 
+{
+public:
+    Node* lowestCommonAncestor(Node* p, Node * q) 
+    {
+        Node *first=p;
+        Node *second=q;
+
+        while(first !=second)
+        {
+            first=first->parent?first->parent:q;
+            second=second->parent?second->parent:p;
+        }
+        return first;
+    }
+};
