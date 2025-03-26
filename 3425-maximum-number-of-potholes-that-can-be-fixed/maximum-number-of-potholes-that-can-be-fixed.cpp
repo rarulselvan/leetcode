ef7f243301ee133868ad6175d5d9f6c8990f7ac5
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-03-26 15:18:43 +0530

    Time: 1699 ms (5.88%) | Memory: 48.7 MB (18.63%) - LeetSync

---
+++ 3425-maximum-number-of-potholes-that-can-be-fixed/maximum-number-of-potholes-that-can-be-fixed.cpp
@@ -0,0 +1,47 @@
+class Solution 
+{
+public:
+    int maxPotholes(string road, int budget) 
+    {
+        vector<int> potholeGrps;
+        int k = 0;
+
+        // Collect consecutive pothole counts
+        for (char x : road)
+        {
+            if (x == 'x')
+                k++;
+            else if (k > 0)
+            {
+                potholeGrps.push_back(k);
+                k = 0;
+            }
+        }
+        if (k > 0) // Capture trailing potholes...x..xxx...(x)--> this x need to be processed
+            potholeGrps.push_back(k);
+
+
+
+        // Sort pothole segments in ascending order (smallest cost to repair first)
+        sort(potholeGrps.rbegin(), potholeGrps.rend());
+        for(auto x: potholeGrps)
+            cout <<x<<" ";
+
+        int repaired = 0;
+        for (int group : potholeGrps) 
+        {
+            if (budget >= group+1) 
+            {
+                repaired += group;
+                budget -= group + 1;
+            }
+            else if (budget > 0)
+            {
+                repaired += budget - 1;  // Partial repair
+                break;
+            }
+            //if (budget <= 0) break;
+        }
+        return repaired;
+    }
+};
