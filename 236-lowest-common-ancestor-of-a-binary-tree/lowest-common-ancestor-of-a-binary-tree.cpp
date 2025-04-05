a5f306664b0597e8a099ea8684eeccbd4fedc305
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-04-05 14:40:02 +0530

    Time: 8 ms (89.73%) | Memory: 17.5 MB (19.41%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -12,12 +12,24 @@ class Solution
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {   
-        if (!root || root == p || root == q) return root;
+        //Base case 
+        if (root==NULL || root == p || root == q) return root;
 
+        //Find the p & q on left and right subtrees
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
-        if (left && right) return root; // If p and q are found in different subtrees
-        return left ? left : right; // Otherwise, return the non-null subtree
+        if (left && right)
+            return root; // If p and q are found in different subtrees
+        else
+        {
+         //   return left ? left : right; // Otherwise, return the non-null subtree
+            if(left)
+                return left;
+            else if(right)
+                return right;
+            else
+                return NULL;
+        }
     }
 };
