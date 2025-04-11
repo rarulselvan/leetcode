1a57072f758c92c112e186ee376c75dea772dfbd
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-04-11 15:01:37 +0530

    Time: 20 ms (26.46%) | Memory: 14.4 MB (23.20%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,33 +1,3 @@
-/*
-class Solution 
-{
-public:
-    int lengthOfLongestSubstring(string s)
-    {
-        unordered_map<char, int> Map;
-        int i=0;
-        int j=0;
-        int res=0;
-        
-        // Sliding window ... j...is right's position.... i is in the left's position
-        for(j=0; j<s.size(); j++)
-        {
-            // check the charactor is present in the map...
-            if(Map.count(s[j])>0)
-            {
-                //now move the i forward till that charactor position ... slide the window
-                i=max(i, Map[s[j]]+1); // max is required to handle "abba" case... if we do not do "max" i  will move backward
-                //i=Map[s[j]]+1;
-                cout <<i<<" ";
-            }
-            res= max(res,j-i+1);//Nunmber of unique charactors ...
-
-            Map[s[j]]=j; //store the charactor and it's position in map...this will be overwritten
-        }
-        return res;
-    }
-};
-*/
 class Solution 
 {
 public:
@@ -51,17 +21,11 @@ public:
                 slidingSet.erase(s[start]);
                 start += 1;
             }
-            //start=0;
 
             // Insert the current character into the set.
             slidingSet.insert(s[end]);
 
-            // Calculate the length of the current substring and update maxLength
-            // if this length is the largest we've found so far.
-            //maxLength = max(maxLength, end - start + 1);
             int currLength=slidingSet.size();
-            cout <<s[end]<<" "<<currLength<<" "<< maxLength<<"    ";
-
             maxLength = max(maxLength, currLength);
         }
         // Return the length of the longest substring found.
