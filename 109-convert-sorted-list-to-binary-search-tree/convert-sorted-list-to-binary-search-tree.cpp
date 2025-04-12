ea8a862b208c31e97d8a2fbf61e5f8841830504a
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-04-12 13:17:25 +0530

    Time: 4 ms (67.00%) | Memory: 33.3 MB (26.11%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -37,14 +37,13 @@ private:
     TreeNode* buildBST(vector<int>& nums, int start, int end)
      {
         if (start <= end) 
-            {
-        
-        int mid = (start + end) / 2;
-        TreeNode* root = new TreeNode(nums[mid]);
-        root->left = buildBST(nums, start, mid - 1);
-        root->right = buildBST(nums, mid + 1, end);
-        return root;
-            }
+        {
+            int mid = (start + end) / 2;
+            TreeNode* root = new TreeNode(nums[mid]);
+            root->left = buildBST(nums, start, mid - 1);
+            root->right = buildBST(nums, mid + 1, end);
+            return root;
+        }
         return NULL;
     }
 };
