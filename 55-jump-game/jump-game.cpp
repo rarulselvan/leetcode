8f55b77a8728dbdf56f2849906c48a3bcb7befbb
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-04-14 15:36:00 +0530

    Time: 2 ms (30.65%) | Memory: 52.2 MB (64.32%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,4 +1,4 @@
-class Solution
+/*class Solution
 {
     vector<int> dp;
 public:
@@ -23,5 +23,20 @@ public:
         }
         dp[start]=reachEnd;
         return reachEnd;
+    }
+};*/
+class Solution 
+{
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int mx = 0;
+        for (int i = 0; i < nums.size(); ++i) 
+        {
+            if (mx < i) 
+                return false;
+            mx = max(mx, i + nums[i]);
+        }
+        return true;
     }
 };
