0cda8c441c29822d8688a566a18917cbfe8bd199
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-04-16 20:41:20 +0530

    Time: 23 ms (32.27%) | Memory: 25.1 MB (63.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,51 +1,33 @@
-/*class Solution {
+class Solution 
+{
 public:
-        string getHash(string &s) 
+    vector<vector<string>> groupAnagrams(vector<string>& strs) 
+    {
+        unordered_map <string, vector<string>>myMap;
+
+        for(string x: strs)
         {
-            string hash;
-            vector<int> freq(26, 0);
-            
-            // Count frequency of each character
-            for(char ch: s)
-                freq[ch - 'a'] += 1;
-            
-            // Append the frequency to construct the hash
-            for(int i = 0; i < 26; i++) {
-                hash.append(to_string(freq[i]));
-                hash.append("$");
-            }
-            
-            return hash;
+            string s=x;
+            sort(s.begin(), s.end());
+            myMap[s].push_back(x); //sorted string is the key into the hash map
         }
 
-        vector<vector<string>> groupAnagrams(vector<string> &arr) 
-        {
-            vector<vector<string>> res;
-            unordered_map<string, int> mp;
-            for (int i = 0; i < arr.size(); i++) {
-                string key = getHash(arr[i]);
-            
-                // If key is not present in the hash map, add
-                // an empty group (vector) in the result and
-                // store the index of the group in hash map
-                if (mp.find(key) == mp.end()) {
-                    mp[key] = res.size();
-                    res.push_back({});
-                }
-            
-                // Insert the string in its correct group
-                res[mp[key]].push_back(arr[i]);
-        }
-        return res;
+        vector<vector<string>> result;
+        for (auto& group : myMap)
+            result.push_back(group.second);
+        return result;
     }
-};*/
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> anagramGroups;
+};
+
+/*
+unordered_map<string, vector<string>> anagramGroups;
+
+        //sorted string -keep it as key
+        //unsorted string - keep it as value
 
         // Iterate through each word
-        for (string s : strs) {
+        for (string s : strs) 
+        {
             string sortedStr = s;
             sort(sortedStr.begin(), sortedStr.end()); // Sort the string
             
@@ -54,10 +36,9 @@ public:
 
         // Collect the grouped anagrams
         vector<vector<string>> result;
-        for (auto& group : anagramGroups) {
+        for (auto& group : anagramGroups) 
             result.push_back(group.second);
-        }
 
         return result;
     }
-};
+    */
