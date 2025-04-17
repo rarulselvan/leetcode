86d07e1032685d3e49b021b34b6e7d6013b9b2af
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-04-17 17:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (21.03%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,35 +1,3 @@
-/*
-class Solution 
-{
-public:
-    int characterReplacement(string s, int k) 
-    {
-        vector<int> charCount(256, 0);
-        int left = 0;
-        int right = 0;
-        int maxCharCount = 0;
-      
-        for (right = 0; right <s.size(); right++)
-        {
-            charCount[s[right]]++; // Increment the count for the current character
-
-            // Update the max frequency character count seen so far in the current window
-            maxCharCount = max(maxCharCount, charCount[s[right]]);
-          
-            // Check if the current window size minus the count of the max frequency character
-            // is greater than k, if so, shrink the window from the left
-            if (right - left + 1 - maxCharCount > k)
-            {
-                charCount[s[left]]--; // Decrement the count for the character at the left index as it's going out of the window
-                left++; // Shrink the window from the left
-            }
-        }
-
-        // The length of the largest window compliant with the condition serves as the answer
-        return right - left;
-    }
-};
-*/
 class Solution 
 {
 public:
@@ -52,6 +20,7 @@ public:
                 charCount[s[left]]--;
                 left++;
             }
+            else
             maxLen = max(maxLen, right - left + 1);
         }
         return maxLen;
