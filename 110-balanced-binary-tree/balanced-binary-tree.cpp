02c8b70506f977f973e38eb5b679a9fc11bba89b
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-04-26 20:16:25 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (57.14%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -14,29 +14,21 @@ class Solution
 public:
     bool isBalanced(TreeNode* root) 
     {
-        int ans = findMaxHeight(root);
-        if(ans ==-1)
+        if(maxHeightDifference(root)==INT_MAX)
             return false;
         else
             return true;
-        
     }
-    //Find the max height of the tree...in that process if abs(leftHeight -RightHeight) is more than 1...it isconculuded theat thre tree is unbalnce and return -1
-    int findMaxHeight(TreeNode* root)
+    int maxHeightDifference(TreeNode* root)
     {
-        if (!root)
+        if(!root)
             return 0;
-        
-        int leftHeight = findMaxHeight(root->left);
-        int rightHeight = findMaxHeight(root->right);
+        int leftHeight=maxHeightDifference(root->left);
+        int rightHeight=maxHeightDifference(root->right);
 
-            if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) 
-            {
-                // If the left or right subtree is unbalanced, or if the current subtree
-                // is unbalanced (difference in height > 1), return -1 as an indicator.
-                return -1;
-            }
+        if (leftHeight == INT_MAX || rightHeight == INT_MAX || abs(leftHeight - rightHeight) > 1) 
+            return INT_MAX;
 
-        return 1 + max(leftHeight, rightHeight);
+        return 1+max(leftHeight,rightHeight);
     }
 };
