a47634b39c5634ec38066a44ca7ce6184fae0aaf
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-04-27 19:34:26 +0530

    Time: 4 ms (9.14%) | Memory: 28.1 MB (25.52%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -16,13 +16,16 @@ public:
     {
         int ans = INT_MIN;
     
-        function<int (TreeNode*)> dfs=[&](TreeNode* root)
+        //function<int (TreeNode*)> dfs=[&](TreeNode* root)
+        function<int (TreeNode *)>dfs;
+        dfs=[&](TreeNode* root)
         {
             if(root==NULL)
                 return 0;
             
-            int leftMax= max(0,dfs(root->left));
-            int rightMax= max(0,dfs(root->right));
+            int leftMax= max(0,dfs(root->left)); //max is required for negative weights
+            int rightMax= max(0,dfs(root->right)); //max is required for negative weights
+
             ans=max(ans, root->val+leftMax+rightMax); //global calculation
             return(max(leftMax, rightMax)+root->val); //this function return value...for the previous caller this function computes left and right subtree and picks the macimum out of it
         };
