37d4e57be41db1190ead95020ea4c63dda06277f
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-05-02 13:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (17.51%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,16 +1,15 @@
-class Solution {
+class Solution 
+{
 public:
     void moveZeroes(vector<int>& nums) 
     {
-        int j = 0; // Pointer for placing non-zero elements
-        
-        // Move all non-zero elements to the front
-        for (int i = 0; i < nums.size(); i++) 
+        int i=0;
+        for(int j=0; j<nums.size(); j++)
         {
-            if (nums[i] != 0) 
+            if(nums[j]!=0)
             {
-                swap(nums[i], nums[j]); // Swap non-zero with zero
-                j++;
+                swap(nums[i], nums[j]);
+                i++;
             }
         }
     }
