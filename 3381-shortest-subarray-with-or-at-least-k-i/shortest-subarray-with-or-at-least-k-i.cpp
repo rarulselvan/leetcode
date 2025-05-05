b3754f5780e865c9746b008acbc4b0807079c06c
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-05-05 16:10:45 +0530

    Time: 5 ms (9.80%) | Memory: 27.6 MB (10.20%) - LeetSync

--- 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.cpp
+++ 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.cpp
@@ -1,23 +1,31 @@
-class Solution {
+class Solution 
+{
 public:
-    int minimumSubarrayLength(vector<int>& nums, int k) {
+    int minimumSubarrayLength(vector<int>& nums, int k) 
+    {
         int n = nums.size();
-        int cnt[32]{};
+        unordered_map<int, int> cnt;
         int ans = n + 1;
-        for (int i = 0, j = 0, s = 0; j < n; ++j) {
-            s |= nums[j];
-            for (int h = 0; h < 32; ++h) {
-                if ((nums[j] >> h & 1) == 1) {
+        int s=0;
+        int i=0;
+
+        for (int j = 0; j < n; j++) 
+        {
+            s = s|nums[j];
+            for (int h = 0; h < 32; ++h) 
+            {
+                if ((nums[j] >> h & 1) == 1)
                     ++cnt[h];
-                }
             }
-            for (; s >= k && i <= j; ++i) {
+            for (; s >= k && i <= j; ++i) 
+            {
                 ans = min(ans, j - i + 1);
-                for (int h = 0; h < 32; ++h) {
-                    if ((nums[i] >> h & 1) == 1) {
-                        if (--cnt[h] == 0) {
+                for (int h = 0; h < 32; ++h) 
+                {
+                    if ((nums[i] >> h & 1) == 1) 
+                    {
+                        if (--cnt[h] == 0)
                             s ^= 1 << h;
-                        }
                     }
                 }
             }
