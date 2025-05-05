8ac14f74919f898a6de78dbaebc0e6a73b63bee8
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-05-05 18:49:31 +0530

    Time: 3 ms (49.32%) | Memory: 26.6 MB (15.54%) - LeetSync

---
+++ 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.cpp
@@ -0,0 +1,23 @@
+
+class Solution 
+{
+public:
+    bool verifyPreorder(vector<int>& preorder) 
+    {
+        stack<int> s;
+        int last = numeric_limits<int>::min();
+
+        for (int value : preorder) 
+        {
+            if (value < last)
+                return false;
+            while (!s.empty() && value > s.top()) 
+            {
+                last = s.top();
+                s.pop();
+            }
+            s.push(value);
+        }
+        return true;
+    }
+};
