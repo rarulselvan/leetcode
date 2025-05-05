f1e568eba8f409b3e5ec97bcabd143f8e722d1ed
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-05-05 11:10:30 +0530

    Time: 178 ms (40.88%) | Memory: 147.3 MB (62.76%) - LeetSync

---
+++ 220-contains-duplicate-iii/contains-duplicate-iii.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) 
+    {
+        multiset<int> window; 
+        for (int i = 0; i < nums.size(); ++i) 
+        {
+            if (i > indexDiff) 
+                window.erase(window.find(nums[i - indexDiff - 1]));
+            
+            auto pos = window.lower_bound((long long)nums[i] - valueDiff);
+            if (pos != window.end() && abs(*pos - nums[i]) <= valueDiff) 
+                return true;
+
+            window.insert(nums[i]);
+        }
+        return false;
+    }
+};
