f1747e855e1c2318be0018217ecea17243c8f88e
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-05-10 12:37:15 +0530

    Time: 2707 ms (5.00%) | Memory: 30.5 MB (25.67%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,26 +1,29 @@
 class Solution 
 {
-private:
-    vector<vector<int>> dp; // Memoization tablepublic:
-
+    int res = 0;
+    vector<vector<int>> dp; // Memoization table:
 public:
-    int changeDP(int i, int amount, vector<int>& coins) 
-    {
-        if (amount == 0) 
-            return 1;  // Found a valid combination
-        if (i >= coins.size() || amount < 0)
-            return 0;  // Invalid case
-
-        if (dp[i][amount] != -1) 
-            return dp[i][amount]; // Return cached result
-
-        // Include current coin OR skip to next coin
-        return dp[i][amount] = changeDP(i, amount - coins[i], coins) + changeDP(i + 1, amount, coins);
-    }
-
     int change(int amount, vector<int>& coins) 
     {
         dp.assign(coins.size(), vector<int>(amount + 1, -1)); // Initialize DP table
-        return changeDP(0, amount, coins); // Start recursion from index 0
+        function<int(int, int)> dfs;
+        dfs = [&](int sum, int start) 
+        {
+            if (sum == 0) 
+                return 1;
+            if (sum < 0)
+                return 0;
+            if (dp[start][sum] != -1) //for thsi starting position and sum..if the result is already computed ..simply use it
+                return dp[start][sum];
+
+            int ways = 0;
+            for (int i = start; i < coins.size(); i++)
+            {
+                ways=ways + dfs(sum - coins[i], i); // Use coin[i] again, so pass i
+            }
+            return dp[start][sum] = ways;
+           // return ways;
+        };
+       return dfs(amount, 0);
     }
 };
