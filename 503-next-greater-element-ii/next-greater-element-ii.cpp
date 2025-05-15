669539ffe709a9de4eb6732b30e66ca2525d9174
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-05-15 11:32:42 +0530

    Time: 5 ms (48.40%) | Memory: 28.6 MB (38.34%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& arr)
+    {
+        int n = arr.size();
+        vector<int> res(n, -1);
+        stack<int> stk;
+
+        // Double the array (since it is a circular array)
+        for (int i = 2 * n - 1; i >= 0; i--) 
+        {
+            // Pop out  the elements from the stack that cannot be the next greater element for the current value
+            while (!stk.empty() && stk.top() <= arr[i % n]) 
+                stk.pop();
+
+            // If the stack is not empty, the top element is the next greater element for the current  element to be added into the stack
+            if (i < n && !stk.empty()) 
+                res[i] = stk.top();
+    
+            // Push the current element onto the stack
+            stk.push(arr[i % n]);
+        }
+        return res;
+    }
+};
