4be80e383b96ab988cfb2ef76e5eb137210b51ed
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-05-17 11:07:19 +0530

    Time: 3 ms (8.43%) | Memory: 9.5 MB (9.02%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,24 +1,49 @@
 class Solution 
 {
+    vector<vector<int>>dp;
 public:
-    int uniquePathsDP(int m, int n, vector<vector<int>> &dp) //receive as reference
+
+    int uniquePaths(int m, int n) 
+    {
+        dp.assign(m, vector<int>(n, -1));  // initialize dp table with -1
+        return uniquePathsDP(m - 1, n - 1);
+    }
+    int uniquePathsDP(int m, int n) 
     {
         if(m==0)
             return 1;
         if(n==0)
             return 1;
+
         if(dp[m][n] !=-1)
             return dp[m][n];
-
-        dp[m][n]=uniquePathsDP(m-1, n, dp)+uniquePathsDP(m, n-1, dp);
+        
+        dp[m][n]=uniquePathsDP(m-1, n)+uniquePathsDP(m, n-1);
         return dp[m][n];
     }
+};
+/*
+class Solution 
+{
+    vector<vector<int>> dp;
+public:
     int uniquePaths(int m, int n) 
     {
-        vector<int> row(n+1, -1);
-	    // Initializing the 2-D vector
-	    vector<vector<int>> dp(m+1, row);
-
-        return uniquePathsDP(m-1, n-1, dp);
+        dp.assign(m, vector<int>(n, -1));  // initialize dp table with -1
+        return uniquePathsDP(m - 1, n - 1);
     }
-};
+
+private:
+    int uniquePathsDP(int i, int j) 
+    {
+        if (i == 0 || j == 0)
+            return 1;  // base case: only one way along the edge
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        // memoized recursion
+        dp[i][j] = uniquePathsDP(i - 1, j) + uniquePathsDP(i, j - 1);
+        return dp[i][j];
+    }
+};*/
