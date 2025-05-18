d03ee16b1cf4bec236967ff1d57eb960865acd3b
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-05-18 13:13:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (91.11%) - LeetSync

---
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int distributeCandies(int n, int limit) 
+    {
+        int count = 0;
+        // Iterate over all possible values for x and y
+        for (int x = 0; x <= limit; ++x)
+        {
+            for (int y = 0; y <= limit; ++y) 
+            {
+                int z = n - x - y;
+                if (0 <= z && z <= limit)
+                    ++count;
+            }
+        }
+        return count;
+    }
+};
