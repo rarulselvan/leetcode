b850a417f4102277693b93c37d34f4784ca6c5da
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-05-18 12:10:40 +0530

    Time: 106 ms (4.05%) | Memory: 73.4 MB (6.32%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,43 +1,29 @@
-class Solution
+class Solution 
 {
-private:
-    vector<vector<int>>dp;
-    int m;
-    int n;
+vector<vector<int>>dp;
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid)
+    int uniquePathsWithObstacles(vector<vector<int>>& mat) 
     {
-        m = grid.size();
-        n = grid[0].size();
-
-        dp.assign(m, vector<int>(n,-1));
-
-        // Call the recursive function starting from (0,0)
-        int result = findPaths(grid, 0, 0);
-
-        return result;
+        int m=mat.size();
+        int n=mat[0].size();
+        dp.assign(m, vector<int>(n, -1));
+        return uniquePathsWithObstaclesDP(mat,m-1, n-1);
     }
-
-    int findPaths(vector<vector<int>> &grid, int i, int j)
+    int uniquePathsWithObstaclesDP(vector<vector<int>>mat, int m, int n)
     {
-        // If out of bounds or there's an obstacle, return 0
-        if (i >= m || j >= n || grid[i][j] == 1) 
+        if(m>=0 && n>=0 && mat[m][n]==1)
             return 0;
         
-        // If reached the bottom-right corner, return 1
-        if (i == m - 1 && j == n - 1) 
+        if(m==0 && n==0)
             return 1;
 
-        // If already calculated, return the stored value
-        if (dp[i][j] != -1) 
-            return dp[i][j];
+        if(m<0|| n <0)
+            return 0;
 
-        // Calculate the number of paths by moving right and down
-        int rightPaths = findPaths(grid, i, j + 1);
-        int downPaths = findPaths(grid, i + 1, j);
+        if(dp[m][n] !=-1)
+            return dp[m][n];
 
-        // Store the result in the memoization table and return it
-        return dp[i][j] = rightPaths + downPaths;
+        dp[m][n]=uniquePathsWithObstaclesDP(mat,m-1,n) + uniquePathsWithObstaclesDP(mat,m,n-1);
+        return dp[m][n];
     }
-
 };
