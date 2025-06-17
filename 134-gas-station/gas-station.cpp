0ca274e58ee76dd36cdf40cdd688d042b25db7ae
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-06-17 16:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (40.68%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -3,23 +3,23 @@ class Solution
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
     {
-        int total_gas = 0, total_cost = 0, tank = 0, start_index = 0;
+        int totalGas = 0, totalCost = 0, tank = 0, startStation = 0;
         
         for (int i = 0; i < gas.size(); i++)
          {
-            total_gas += gas[i];
-            total_cost += cost[i];
-            tank = tank +(gas[i] - cost[i]);
+            totalGas    += gas[i];
+            totalCost   += cost[i];
+            tank        = tank +(gas[i] - cost[i]);
             
             // If tank is negative, we cannot reach the next station
             if (tank < 0)
             {
                 // Which means if you start from this station you cannot move forward hence this station can't be the starting point
-                start_index = i + 1;  // Set next station as the new start
+                startStation = i + 1;  // Set next station as the new start
                 tank = 0;  // Reset tank
             }
         }
         //compare the total cost and the total fuel
-        return (total_gas >= total_cost) ? start_index : -1;
+        return (totalGas >= totalCost) ? startStation : -1;
     }
 };
