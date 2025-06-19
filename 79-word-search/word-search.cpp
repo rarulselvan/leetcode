f03847f4a063f3f060c89f5406e61392e213d098
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-06-19 12:39:33 +0530

    Time: 801 ms (33.95%) | Memory: 11.8 MB (18.97%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -15,20 +15,25 @@ public:
         if((board[row][col] != word[index]) || (board[row][col] == '#'))
             return false;
 
-        // this is to prevent reusing of the same character
+        // this is to prevent reusing(avoid loop) of the same character
         char c = board[row][col];
         board[row][col] = '#';
+        
+        bool top=false;
+        bool right=false;
+        bool bottom=false;
+        bool left=false;
 
         // top direction
-        bool top = searchNext(board, word, row - 1, col, index + 1, m, n);
-        // right direction
-        bool right = searchNext(board, word, row, col + 1, index + 1, m, n);
-        // bottom direction
-        bool bottom = searchNext(board, word, row + 1, col, index + 1, m, n);
-        // left direction
-        bool left = searchNext(board, word, row, col - 1, index + 1, m, n);
+        top = searchNext(board, word, row - 1, col, index + 1, m, n);
+        if(!top)
+            right = searchNext(board, word, row, col + 1, index + 1, m, n);
+        if(!right)
+            bottom = searchNext(board, word, row + 1, col, index + 1, m, n);
+        if(!bottom)
+            left = searchNext(board, word, row, col - 1, index + 1, m, n);
 
-        board[row][col] = c; // undo change
+        board[row][col] = c; // restore the charactor
 
         return top || right || bottom || left;
     }
@@ -39,13 +44,12 @@ public:
 
         int index = 0;
 
-        // First search the first character
         for (int i = 0; i < m; i++) 
         {
             for (int j = 0; j < n; j++) 
             {
 
-                if (board[i][j] == word[index])
+                if (board[i][j] == word[index]) //try to match the first charactor in the matrix
                 {
                     if (searchNext(board, word, i, j, index, m, n))
                         return true;
