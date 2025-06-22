16879623316b07fde7598b72a0a4a7005c7bc3f7
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-06-22 11:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (64.17%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,41 +1,15 @@
-/*class Solution
-{
-    vector<int> dp;
-public:
-    bool canJump(vector<int>& nums) 
-    {
-        dp.assign(nums.size(), -1);
-        return canJumpDP(nums, 0, nums.size()-1);    
-    }
-    bool canJumpDP(vector<int>& nums, int start, int end)
-    {
-        if(start>=end)
-            return true;
-        if(dp[start] !=-1)
-            return dp[start];// -1 .. 0...1
 
-        bool reachEnd=false;
-        for(int i=1; i<=nums[start]; i++)
-        {
-            reachEnd=canJumpDP(nums, start+i, end);
-            if(reachEnd==true)
-                break;
-        }
-        dp[start]=reachEnd;
-        return reachEnd;
-    }
-};*/
 class Solution 
 {
 public:
     bool canJump(vector<int>& nums) 
     {
-        int mx = 0;
+        int maxReachable = 0;
         for (int i = 0; i < nums.size(); i++) //TRy every index
         {
-            if (mx < i)  //if maximum reachable is less than the current then not possible
+            if (maxReachable < i)  //if maximum reachable is less than the current then not possible
                 return false;
-            mx = max(mx, i + nums[i]);
+            maxReachable = max(maxReachable, i + nums[i]);
         }
         return true;
     }
