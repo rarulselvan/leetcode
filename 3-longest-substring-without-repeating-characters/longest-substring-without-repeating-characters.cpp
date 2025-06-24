511c64697ea26d02a2e533a16d37bfe9412d8b1e
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-06-24 17:23:21 +0530

    Time: 19 ms (30.65%) | Memory: 12.3 MB (42.33%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -15,6 +15,8 @@ public:
             while(Map[s[right]]>1)
             {
                 Map[s[left]]--;
+                if(Map[s[left]]==0)
+                    Map.erase(s[left]);
                 left++;
             }
             len=max(len, right-left+1);
