ed8a8acd7f5dd17e6f0d9aea15a2339edb0059da
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-06-27 17:06:58 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (14.27%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -9,34 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
- 
-    void flatten( TreeNode* root)
+class Solution 
 {
-    stack< TreeNode *>st;
-    st.push(root);
-    // If the left subtree was flattened, merge it with the current node
-    // If the right subtree was flattened, return its tail node
-    // If neither subtree was flattened, return the current node as the tail node
-    while(!st.empty())
+public:
+    void flatten( TreeNode* root)
     {
-         TreeNode *curr = st.top();
-        st.pop();
+        stack< TreeNode *>st;
+        st.push(root);
+        // If the left subtree was flattened, merge it with the current node
+        // If the right subtree was flattened, return its tail node
+        // If neither subtree was flattened, return the current node as the tail node
+        while(!st.empty())
+        {
+            TreeNode *curr = st.top();
+            st.pop();
 
-        if(curr==NULL) 
-            return;
-            
-        if(curr->right!=NULL) 
-            st.push(curr->right);
-        if(curr->left!=NULL) 
-            st.push(curr->left);
+            if(curr==NULL) 
+                return;
+                
+            if(curr->right!=NULL) 
+                st.push(curr->right);
+            if(curr->left!=NULL) 
+                st.push(curr->left);
 
-        if(st.empty()!=true)// Set the left child of the current node to null
-            curr->right = st.top();
+            if(st.empty()!=true)// Set the left child of the current node to null
+                curr->right = st.top();
 
-        curr->left = NULL;
+            curr->left = NULL;
+        }
+        return;
     }
-    return;
-}
 };
