52ac2554d9cba0bf6a3347a4c6fb86ad6775fe18
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-07-03 11:22:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (30.62%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    string gcdOfStrings(string str1, string str2) 
+    {
+        if(str1+str2 != str2+str1)
+            return "";
+        
+        int length=gcd(str1.size(), str2.size());
+
+       // return (str1.substr(0, length));//Both are correct
+        return (str2.substr(0, length));
+        
+    }
+};
