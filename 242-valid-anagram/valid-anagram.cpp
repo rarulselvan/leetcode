f36bfd2b834810e47f2a1357082a2edc0bc7e9f1
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-07-29 12:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (26.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,36 @@
-class Solution {
+/*class Solution 
+{
 public:
     bool isAnagram(string s, string t)
     {
-        if(s.size()!=t.size())
+        if(s.size() !=t.size())
             return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         if(s==t)
             return true;
-
-        return false;
+        else
+            return false;
+    }
+};
+*/
+class Solution 
+{
+public:
+    bool isAnagram(string s, string t)
+    {
+        unordered_map <char, int>Map1;
+        unordered_map <char, int>Map2;
+        if(s.size() !=t.size())
+            return false;
+        for(auto x: s)
+            Map1[x]++;
+        for(auto x: t)
+            Map2[x]++;
+            
+        if(Map1==Map2)
+            return true;
+        else
+            return false;
     }
 };
