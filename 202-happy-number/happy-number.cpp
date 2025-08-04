9ec5e8a7d5af0d1474756107c30b29f2ab846067
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-08-04 20:24:32 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (75.19%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -3,16 +3,43 @@ class Solution
 public:
     bool isHappy(int n) 
     {
-        unordered_set<int> vis;
-        while (n != 1 && !vis.count(n)) 
+        // Lambda function to calculate the next number by summing the squares of the digits
+        auto getNextNumber = [](int currentNumber) 
         {
-            vis.insert(n);
-            int x = 0;
-            for (; n; n /= 10) 
-                x += (n % 10) * (n % 10);
-            
-            n = x;
+            int sumOfSquares = 0;
+            while (currentNumber > 0) 
+            {
+                int digit = currentNumber % 10; // Get last digit
+                sumOfSquares    = sumOfSquares + pow(digit, 2); // Add square of the digit to sum
+                currentNumber   = currentNumber/10; // Remove the last digit
+            }
+            return sumOfSquares;
+        };
+      
+        // Initialize two pointers for the cycle detection (Floyd's ALgorithm)
+        int slowPointer = n; // Slow pointer moves one step
+        int fastPointer = n;//getNextNumber(n); // Fast pointer moves two steps
+
+        while(true)
+        {
+            slowPointer=getNextNumber(slowPointer);
+            fastPointer=getNextNumber(getNextNumber(fastPointer));
+            if(fastPointer==1)
+                return true;
+            else if(slowPointer==fastPointer)
+                return false;
+        }      
+      /*
+        while (slowPointer != fastPointer) 
+        {
+            slowPointer = getNextNumber(slowPointer);
+            fastPointer = getNextNumber(getNextNumber(fastPointer));
         }
-        return n == 1;
+
+        // If slowPointer is 1, n is a happy number
+        if(slowPointer == 1)
+            return true;
+        else
+            return false;*/
     }
 };
