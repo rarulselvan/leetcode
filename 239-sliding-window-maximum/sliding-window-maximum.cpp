38e19b00c37a7514b93c1c2d646e9b80399f3c6d
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-08-06 16:14:00 +0530

    Time: 71 ms (15.00%) | Memory: 153.1 MB (20.57%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,33 +1,38 @@
-
 class Solution 
 {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) 
     {
-        deque<int> windowIndices;  // Deque to store indices of elements in the current window
-        vector<int> ans;     // Vector to store the maximum value for each window
+        int n = arr.size();
 
-        for (int i = 0; i < nums.size(); ++i) 
-        {
-            if( windowIndices.front()==i-k)
-                windowIndices.pop_front();        // Remove indices of elements not in the current window
-            
-            // Maintain the elements in decreasing order in the deque, maximum value will be there in the front 
-            // Pop elements from the back that are less than or equal to the current element
-            while (!windowIndices.empty() && nums[windowIndices.back()] <= nums[i]) 
-                windowIndices.pop_back();
-            
-            // Push current element's index onto the deque
-            windowIndices.push_back(i);
-          
-            // If we've reached the end of the first window, record the max for the current window
-            if (i >= k-1 )
-            {
-                cout <<i<<" "<<k<<endl;
-                ans.push_back(nums[windowIndices.front()]);
-            }
+        // to store the results
+        vector<int> res;
+
+        // to store the max value
+        priority_queue<pair<int, int> > heap;
+
+        // Initialize the heap with the first k elements
+        for (int i = 0; i < k; i++)
+            heap.push({ arr[i], i });
+
+        // The maximum element in the first window
+        res.push_back(heap.top().first);
+
+        // Process the remaining elements
+        for (int i = k; i < arr.size(); i++) {
+
+            // Add the current element to the heap
+            heap.push({ arr[i], i });
+
+            // Remove elements that are outside the current
+            // window
+            while (heap.top().second <= i - k)
+                heap.pop();
+
+            // The maximum element in the current window
+            res.push_back(heap.top().first);
         }
-    
-        return ans;  // Return the list of maximum values
+
+        return res;
     }
 };
