1adec22da94ca545032c797befb42405cd815482
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-08-15 21:13:24 +0530

    Time: 222 ms (42.02%) | Memory: 14.4 MB (52.12%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,58 @@
+class Solution 
+{
+private:
+    vector<int>dp;
+public:
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        dp.resize(nums.size()+1,-1);
+        int res=0;
+
+        for(int i=0; i<nums.size(); i++)
+            lisEndAtI(nums, i);
+        
+        for(int i=0; i<nums.size();i++)
+            res=max(res, dp[i]);        
+        return res;
+    }
+    int lisEndAtI(vector<int> &nums, int i)
+    {
+        if (dp[i] != -1) 
+            return dp[i];
+        
+        int mx = 1; //start with one because every increasing sequence there are two numbers
+
+        for (int prev = 0; prev <i; prev++)
+        {
+            if (nums[prev] < nums[i])
+                mx = max(mx, 1+lisEndAtI(nums, prev));
+        }
+        cout <<i <<" "<<mx<<endl;
+        dp[i]=mx;
+        return mx;
+    }
+};
+
+/*
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1); // dp[i] = LIS ending at i
+
+        int res = 1;
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < i; j++) 
+            {
+                if (nums[j] < nums[i]) 
+                {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
+*/
