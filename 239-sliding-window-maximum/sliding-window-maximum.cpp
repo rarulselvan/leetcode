faeb25da21e70ef174da4dc186c766eda8fda256
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-08-18 16:37:13 +0530

    Time: 69 ms (15.48%) | Memory: 153.1 MB (20.31%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,6 +1,7 @@
 class Solution 
 {
 public:
+
     vector<int> maxSlidingWindow(vector<int>& arr, int k) 
     {
         int n = arr.size();
@@ -23,8 +24,8 @@ public:
             // Add the next element
             heap.push({ arr[i], i });
 
-            // Remove elements that are outside the current
-            // window
+            // Remove top element(s) that are outside the current window...heap size may grow beyond the sliding window size
+            //Even though the heap may grow beyond k, you lazy-remove old elements only when they appear at the top.
             while (heap.top().second <= i - k)
                 heap.pop();
 
@@ -33,5 +34,41 @@ public:
         }
 
         return res;
+    } 
+};
+//Monototonic stack implementation
+
+/*
+class Solution 
+{
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+    {
+        deque<int> windowIndices;  // Deque to store indices of elements in the current window
+        vector<int> ans;     // Vector to store the maximum value for each window
+
+        for (int i = 0; i < nums.size(); ++i) 
+        {
+            if( windowIndices.front()==i-k)
+                windowIndices.pop_front();        // Remove indices of elements not in the current window
+            
+            // Maintain the elements in decreasing order in the deque, maximum value will be there in the front 
+            // Pop elements from the back that are less than or equal to the current element
+            while (!windowIndices.empty() && nums[windowIndices.back()] <= nums[i]) 
+                windowIndices.pop_back();
+            
+            // Push current element's index onto the deque
+            windowIndices.push_back(i);
+          
+            // If we've reached the end of the first window, record the max for the current window
+            if (i >= k-1 )
+            {
+                cout <<i<<" "<<k<<endl;
+                ans.push_back(nums[windowIndices.front()]);
+            }
+        }
+    
+        return ans;  // Return the list of maximum values
     }
 };
+*/
