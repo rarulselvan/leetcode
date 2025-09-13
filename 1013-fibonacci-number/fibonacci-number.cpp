cd332d21ec4c0a821628748804e3f53eae605bc5
Arul Selvan R <31684651+rarulselvan@users.noreply.github.com>
2025-09-13 19:24:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (20.05%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+private:
+    vector<int>dp;
+public:
+   
+    int fib(int n)
+    {
+        dp.assign(n+1, -1);
+        return fibcal(n);
+    }
+    int fibcal(int n)
+    {
+        
+        if(n<=0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(dp[n] !=-1)
+        {
+            cout <<"already computed..."<<endl;
+            return dp[n];
+        }
+        return dp[n]=fibcal(n-1)+fibcal(n-2);
+    }
+};
